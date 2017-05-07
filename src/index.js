3383824caaf1c5e727e90a443ad770a604785c95
Ilija Vankov <vankov.ilija@gmail.com>
2017-05-07 14:25:47 +0200

    export data provider

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import ReactContext from "./context"
 import ReactView from "./view"
 import onStateChange from "./on-state-change"
 import automediate from "./automediate"
+import DataProvider from "./data-provider"
 
-export {ReactContext, ReactView, onStateChange, automediate};
+export {ReactContext, ReactView, onStateChange, automediate, DataProvider};
